d5ed5412308f31396d2169270fb05c0f785c2569
Howard Smith <hj_smith@live.com>
2024-10-19 21:47:04 +0100

    test: Refactor tests to use constants

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -4,28 +4,24 @@ import responses
 from responses import matchers
 
 from cron_mon import monitor
+from tests.constants import API_FINISH_URL, API_START_URL, MONITOR_ID
 
 
 @responses.activate
 def test_monitoring() -> None:
     """Test that the `monitor` decorator works as expected."""
-    monitor_id = "a04376e2-0fb5-4949-9744-7c5d0a50b411"
-    start_api_url = (
-        f"http://mock.cron-mon.io/api/v1/monitors/{monitor_id}/jobs/start"
-    )
-    finish_api_url = f"http://mock.cron-mon.io/api/v1/monitors/{monitor_id}/jobs/12345/finish"
     responses.post(
-        start_api_url,
+        API_START_URL,
         json={"data": {"job_id": "12345"}},
         match=[matchers.header_matcher({"X-API-Key": "mock-api-key"})],
     )
     responses.post(
-        finish_api_url,
+        API_FINISH_URL,
         json={"data": {}},
         match=[matchers.header_matcher({"X-API-Key": "mock-api-key"})],
     )
 
-    @monitor(monitor_id)
+    @monitor(MONITOR_ID)
     def cron_job() -> str:
         """A sample cron job."""
         return "Hello, world!"
@@ -33,6 +29,6 @@ def test_monitoring() -> None:
     cron_job()
 
     assert [c.request.url for c in responses.calls] == [
-        "http://mock.cron-mon.io/api/v1/monitors/a04376e2-0fb5-4949-9744-7c5d0a50b411/jobs/start",
-        "http://mock.cron-mon.io/api/v1/monitors/a04376e2-0fb5-4949-9744-7c5d0a50b411/jobs/12345/finish",
+        API_START_URL,
+        API_FINISH_URL,
     ]
